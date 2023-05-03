be3a188bd8fe22aaf55a914a34de72fd2dcdf8c1
iqbalhossensorkar <iqbalhossen303@gmail.com>
2023-05-04 00:50:24 +0600

    Json file structure changed

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const port = process.env.PORT || 5000
 
 const chefs = require('./data/chefs.json')
+const recipe = require('./data/recipies.json')
 
 app.use(cors());
 
@@ -22,6 +23,11 @@ app.get('/chefs/:id', (req, res) => {
     res.send(selectedChef)
 })
 
+app.get('/chefs/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const allRecipe = recipe.filter(r => parseInt(r.recipe_id) === id);
+    res.send(allRecipe);
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
